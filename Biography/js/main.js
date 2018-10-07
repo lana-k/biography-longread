7309f3b820df09b34a9bca9c7b84e0881cf0abaf
lana-k <skochetova89@gmail.com>
2018-10-07 22:28:09 +0200

    refine js

--- Biography/js/main.js
+++ Biography/js/main.js
@@ -1,46 +1,39 @@
 $(document).ready(function(){
-    console.log("123");
-    var controller = new ScrollMagic.Controller();
+            var controller = new ScrollMagic.Controller();
             
             var scene = new ScrollMagic.Scene({
-                triggerElement:".part1",
+                triggerElement: ".part1",
                 triggerHook: 0.0
-                
             })
             .setClassToggle(".chapter1", "sticky");
             
             var scene2 = new ScrollMagic.Scene({
-                triggerElement:".part2",
+                triggerElement: ".part2",
                 triggerHook: 0.0
-                
             })
             .setClassToggle(".chapter2", "sticky");
             
-             var scene3 = new ScrollMagic.Scene({
-                triggerElement:".part3",
+            var scene3 = new ScrollMagic.Scene({
+                triggerElement: ".part3",
                 triggerHook: 0.0
-                
             })
             .setClassToggle(".chapter3", "sticky");
             
-             var scene4 = new ScrollMagic.Scene({
-                triggerElement:".part4",
+            var scene4 = new ScrollMagic.Scene({
+                triggerElement: ".part4",
                 triggerHook: 0.0
-                
             })
             .setClassToggle(".chapter4", "sticky");
             
-             var scene5 = new ScrollMagic.Scene({
-                triggerElement:".part5",
+            var scene5 = new ScrollMagic.Scene({
+                triggerElement: ".part5",
                 triggerHook: 0.0
-                
             })
             .setClassToggle(".chapter5", "sticky");
             
-             var scene6 = new ScrollMagic.Scene({
-                triggerElement:".part6",
+            var scene6 = new ScrollMagic.Scene({
+                triggerElement: ".part6",
                 triggerHook: 0.0
-                
             })
             .setClassToggle(".chapter6", "sticky");
             
